bf3ef773044873351a7bedec41d4faa932bc346a
ALAN ANTO <89739433+alankudakkad17@users.noreply.github.com>
2023-06-04 21:24:07 +0530

    Add files via upload

---
+++ fact.R
@@ -0,0 +1,23 @@
+num<-readline("Enter the number:")
+fact<-1
+i<-1
+for(i in 1:num)
+{
+  fact=fact*i
+}
+cat("factorial is:",fact,"\n")
+fcopy<-fact
+n_num<-new<-0
+while(fact>0)
+{
+  n_num=n_num*10+fact%%10
+  fact<-fact/10
+}
+
+if(fcopy==new)
+{
+  cat(fcopy,"is palindrome")
+}
+else{
+  cat(fcopy,"is not palindrome")
+}
